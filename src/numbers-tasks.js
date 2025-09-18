db52af6f34ddf4adc6e760b98e48f44f763f5356
aliakseidrob <aliakseidrob@gmail.com>
2025-09-18 21:37:39 +0300

    feat: solution part 2

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -153,8 +153,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(b ** 2 + c ** 2 + a ** 2);
 }
 
 /**
@@ -174,8 +174,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -195,8 +195,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(num) {
+  const numSqrt = Math.sqrt(num);
+  for (let i = 2; i <= numSqrt; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
 }
 
 /**
